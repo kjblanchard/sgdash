77431d59b92700ac55547631b5403e3bad2315ab
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-02-17 22:30:21 -0500

    Refactor collision

--- src/systems/collision_system.hpp
+++ src/systems/collision_system.hpp
@@ -8,16 +8,16 @@ class BoxColliderComponent;
 class CollisionSystem
 {
 public:
-    static void Update(entt::registry &reg, const double &delta_time);
-    static bool check_collision(entt::registry &reg, entt::entity &ent, BoxColliderComponent& box_collider, TransformComponent& transform);
+    static bool check_collision(entt::registry &reg, entt::entity &ent, BoxColliderComponent &box_collider, TransformComponent &transform);
 
-    static inline bool aabb_collision_check(SDL_Rect& lhs, SDL_Rect& rhs){
-        return SDL_HasIntersection(&lhs,&rhs);
+    static inline bool aabb_collision_check(SDL_Rect &lhs, SDL_Rect &rhs)
+    {
+        return SDL_HasIntersection(&lhs, &rhs);
     }
-    static inline SDL_Rect aabb_collision_area(SDL_Rect& lhs, SDL_Rect& rhs){
+    static inline SDL_Rect aabb_collision_area(SDL_Rect &lhs, SDL_Rect &rhs)
+    {
         SDL_Rect overlap;
         SDL_IntersectRect(&lhs, &rhs, &overlap);
         return overlap;
-
     }
 };
